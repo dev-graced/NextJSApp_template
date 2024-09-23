cddb28de56cc9553a097530737df06ca7f94f563
dev-graced <110510211+dev-graced@users.noreply.github.com>
2024-09-23 01:25:48 +0000

    feat: create/route.js に nextAppDataBase にレコードを書き込む機能を追加

--- app/api/item/create/route.js
+++ app/api/item/create/route.js
@@ -1,10 +1,24 @@
 import { NextResponse } from "next/server";
 import connectDB from "../../../utils/database";
 
+import {ItemModel} from "../../../utils/schemaModels";
+
 export async function POST(request) {
 
-    console.log(await request.json())
-    connectDB()
-    return NextResponse.json({message:"アイテム作成"})
+    const reqBody = await request.json();
+    // console.log(reqBody);
+
+    try{
+        // console.log(await request.json());
+
+        await connectDB();
+
+        await ItemModel.create(reqBody);
+
+        return NextResponse.json({message:"アイテム作成成功"});
+
+    }catch{
+        return NextResponse.json({message:"アイテム作成失敗"});
+    }
     
 }                                                                                                                                   
