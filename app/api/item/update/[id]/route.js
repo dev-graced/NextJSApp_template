bfacdebc886923ff2cc1f0eb483fd5801b505c83
dev-graced <110510211+dev-graced@users.noreply.github.com>
2024-09-24 12:58:19 +0000

    feat: request に含まれる email で照合して、OKなら編集を許可する機能を追加

--- app/api/item/update/[id]/route.js
+++ app/api/item/update/[id]/route.js
@@ -1,7 +1,5 @@
 import { NextResponse } from "next/server";
-
 import connectDB from "../../../../utils/database";
-
 import {ItemModel} from "../../../../utils/schemaModels";
 
 export async function PUT(request,context) {
@@ -9,13 +7,20 @@ export async function PUT(request,context) {
     // console.log(context.params.id);
     const reqBody = await request.json();
 
+    /**  アイテム編集の処理　*/
     try{
         await connectDB();
 
-        const singleItem = await ItemModel.updateOne({_id: context.params.id},reqBody);
+        const singleItem =  await ItemModel.findById(context.params.id);
 
+        if(singleItem.email === reqBody.email){
+            await ItemModel.updateOne({_id: context.params.id},reqBody);
 
-        return NextResponse.json({message: "アイテム編集成功"});
+            return NextResponse.json({message: "アイテム編集成功"});
+        }else{
+            return NextResponse.json({message: "他の人が編集したアイテムです"});
+        }
+
     }catch{
         return NextResponse.json({message: "アイテム編集失敗"});
     }
