c884673da80fac21896cb72b33b08fc23db41282
dev-graced <110510211+dev-graced@users.noreply.github.com>
2024-09-27 09:20:39 +0000

    feat: ホームページにアクセスすると、すべてのアイテムを読み込む機能を追加

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,22 @@
 // "use client"
 
-const ReadAllItems = () => {
+const getAllItems = async()=>{
+  const response = await fetch("https://expert-broccoli-jv79rjjwxpp256jw-3000.app.github.dev/api/item/readall");
+
+  const jsonData = await response.json();
+
+  // console.log(jsonData.allItems);
+
+  const allItems = jsonData.allItems;
+
+  return allItems;
+}
+
+const ReadAllItems = async() => {
+  const allItems = await getAllItems();
+
+  console.log(allItems);
+
   return(
     <div>
       <h1 className="h1-style">こんにちは</h1>
